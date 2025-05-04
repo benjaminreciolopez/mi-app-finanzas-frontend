047f5eb068958b84231340f77138b9f229d05a8b
Benjamin <benjaminreciolopez@hotmail.es>
2025-05-04 08:20:10 +0200

    ✨ Añadido swipe horizontal y transición suave entre pestañas, actualizada App y main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // ← así importa App.tsx
+import App from "./App";
+import { NavigationDirectionProvider } from "./NavigationDirectionContext"; // 👈
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <NavigationDirectionProvider>
+      <App />
+    </NavigationDirectionProvider>
   </React.StrictMode>
 );
