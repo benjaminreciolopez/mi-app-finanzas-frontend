d6d23a286e3312ab19f66dd14bc6a3e10357a4df
Benjamin <benjaminreciolopez@hotmail.es>
2025-06-15 23:15:50 +0200

    Modo mantenimiento

--- vite.config.ts
+++ vite.config.ts
@@ -37,48 +37,17 @@ export default defineConfig({
             icons: [{ src: "/icon_128x128.png", sizes: "128x128" }],
           },
         ],
-
         icons: [
-          {
-            src: "/icon_16x16.png",
-            sizes: "16x16",
-            type: "image/png",
-          },
-          {
-            src: "/icon_32x32.png",
-            sizes: "32x32",
-            type: "image/png",
-          },
-          {
-            src: "/icon_48x48.png",
-            sizes: "48x48",
-            type: "image/png",
-          },
-          {
-            src: "/icon_64x64.png",
-            sizes: "64x64",
-            type: "image/png",
-          },
-          {
-            src: "/icon_128x128.png",
-            sizes: "128x128",
-            type: "image/png",
-          },
-          {
-            src: "/icon_256x256.png",
-            sizes: "256x256",
-            type: "image/png",
-          },
-          {
-            src: "/icon_512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/icon_1024x1024.png",
-            sizes: "1024x1024",
-            type: "image/png",
-          },
+          { src: "/icon_16x16.png", sizes: "16x16", type: "image/png" },
+          { src: "/icon_32x32.png", sizes: "32x32", type: "image/png" },
+          { src: "/icon_48x48.png", sizes: "48x48", type: "image/png" },
+          { src: "/icon_64x64.png", sizes: "64x64", type: "image/png" },
+          { src: "/icon_128x128.png", sizes: "128x128", type: "image/png" },
+          { src: "/icon_192x192.png", sizes: "192x192", type: "image/png" },
+          { src: "/icon_256x256.png", sizes: "256x256", type: "image/png" },
+          { src: "/icon_384x384.png", sizes: "384x384", type: "image/png" },
+          { src: "/icon_512x512.png", sizes: "512x512", type: "image/png" },
+          { src: "/icon_1024x1024.png", sizes: "1024x1024", type: "image/png" },
         ],
       },
       workbox: {
@@ -93,7 +62,7 @@ export default defineConfig({
               networkTimeoutSeconds: 3,
               expiration: {
                 maxEntries: 50,
-                maxAgeSeconds: 60 * 60, // 1 hora
+                maxAgeSeconds: 3600,
               },
               cacheableResponse: {
                 statuses: [0, 200],
