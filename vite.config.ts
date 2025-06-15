bb0d638670e264aa4df05e59af390429216e476f
Benjamin <benjaminreciolopez@hotmail.es>
2025-06-15 23:16:21 +0200

    Modo mantenimiento

--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,6 @@ export default defineConfig({
           { src: "/icon_48x48.png", sizes: "48x48", type: "image/png" },
           { src: "/icon_64x64.png", sizes: "64x64", type: "image/png" },
           { src: "/icon_128x128.png", sizes: "128x128", type: "image/png" },
-          { src: "/icon_192x192.png", sizes: "192x192", type: "image/png" },
           { src: "/icon_256x256.png", sizes: "256x256", type: "image/png" },
           { src: "/icon_384x384.png", sizes: "384x384", type: "image/png" },
           { src: "/icon_512x512.png", sizes: "512x512", type: "image/png" },
