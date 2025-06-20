066cd2bb701f438d0f824fa81c0dead68ed70341
Benjamin <benjaminreciolopez@hotmail.es>
2025-06-20 20:39:31 +0200

    Modo mantenimiento

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { NavigationDirectionProvider } from "./NavigationDirectionContext";
 import { SwipeDirectionProvider } from "./components/navigation/SwipeDirectionContext";
-import { BrowserRouter } from "react-router-dom"; // ⬅️ importa esto
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      {" "}
-      {/* ⬅️ envuelve todo dentro */}
-      <NavigationDirectionProvider>
-        <SwipeDirectionProvider>
-          <App />
-        </SwipeDirectionProvider>
-      </NavigationDirectionProvider>
-    </BrowserRouter>
+    <NavigationDirectionProvider>
+      <SwipeDirectionProvider>
+        <App />
+      </SwipeDirectionProvider>
+    </NavigationDirectionProvider>
   </React.StrictMode>
 );
